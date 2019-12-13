42b809e827ba93ff71153022ed5e62ad787df451
Hamoud Mohamed <hamoudmohamed@MacBook-Air-de-Hamoud.local>
2019-12-13 15:24:01 +0100

    modified Yeux and Capteur

--- Yeux.h
+++ Yeux.h
@@ -14,6 +14,7 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+#define PI 3.14159265
 
 using namespace std;
 
@@ -21,8 +22,26 @@ class Yeux: public Capteur {
     public:
         Yeux(Bestiole *laBestiole): Capteur(laBestiole){}
     void update(){
-        
-        
-    }
-}
+        double xBest = getBestiole()->getX();
+        double yBest = getBestiole()->getY();
+        double orient = getBestiole()->getOrientation();
+        double deltaY = this.deltaY;
+        double alpha = this.alpha;
+        std::vector<Bestiole> newListV;
+        std::vector<Bestiole> listB= getBestiole()->getMilieu()->getListeBestioles();
+        double probVoir = static_cast<double>( rand() )/RAND_MAX;
+        for ( std::vector<Bestiole>::iterator it = listeB.begin() ; it != listeB.end() ; ++it )
+        {
+            double xCur = it->getX();
+            double yCur = it->getY();
+            double angle = (atan2(yCur-yBest,xCur-xBest)*180 / PI)-orient;
+            if (angle<alpha/2){
+                double dist = sqrt(pow(yCur-yBest,2)+pow(xCur-xBest,2));
+                if (dist<deltaY){
+                    newListV.push_back(*it);
+                };
+            };
+        };
+    };
+};
 
