8ea229945ed5892399e0f583c1243a0304eff5c2
Khalid MORO <k18moro@pc-df-806.priv.enst-bretagne.fr>
2019-12-13 12:23:07 +0100

    add birth and die process

--- Milieu.cpp
+++ Milieu.cpp
@@ -3,14 +3,15 @@
 #include <cstdlib>
 #include <ctime>
 
-
+#include "Bestiole.h"
 const T    Milieu::white[] = { (T)255, (T)255, (T)255 };
 
 
 Milieu::Milieu( int _width, int _height ) : UImg( _width, _height, 1, 3 ),
                                             width(_width), height(_height)
 {
-
+   deathProbability=0.01;
+   birthProbability=0.01;
    cout << "const Milieu" << endl;
 
    std::srand( time(NULL) );
@@ -27,12 +28,23 @@ Milieu::~Milieu( void )
 
 
 void Milieu::step( void )
-{
-
+{ 
+// Add new born bestioles
+  float probaOfBirth=((double) std::rand() / (RAND_MAX)); 
+  if (probaOfBirth < this->birthProbability){
+    
+      addMember(Bestiole());
+   }
+// Remove dead bestioles
+  float probaOfDeath=((double) std::rand() / (RAND_MAX)); 
+  if (probaOfDeath < this->deathProbability){
+    
+      removeMember();
+   }
    cimg_forXY( *this, x, y ) fillC( x, y, 0, white[0], white[1], white[2] );
    for ( std::vector<Bestiole>::iterator it = listeBestioles.begin() ; it != listeBestioles.end() ; ++it )
    {
-
+    
       it->action( *this );
       it->draw( *this );
 
--- Milieu.h
+++ Milieu.h
@@ -4,6 +4,9 @@
 
 #include "UImg.h"
 #include "Bestiole.h"
+#include <cstdlib>
+#include <cmath>
+#include <stdlib.h>  
 
 #include <iostream>
 #include <vector>
@@ -25,16 +28,28 @@ private :
    float delta0min, delta0max;
    float gamma0min, gamma0max;
 
+
 public :
    Milieu( int _width, int _height );
    ~Milieu( void );
+   float get_deathProbability( void ) const { return deathProbability;};
+   float get_birthProbability( void ) const { return birthProbability;};
+   float get_alphaMin( void ) const { return alphaMin;};
+   float get_alphaMax( void ) const { return alphaMax;};
+   float get_deltaYmin( void ) const { return deltaYmin;};
+   float get_deltaYmax( void ) const { return deltaYmax;};
+   float get_delta0min( void ) const { return delta0min;};
+   float get_delta0max( void ) const { return delta0max;};
+   float get_gamma0min( void ) const { return gamma0min;};
+   float get_gamma0max( void ) const { return gamma0max;};
 
    int getWidth( void ) const { return width; };
    int getHeight( void ) const { return height; };
 
    void step( void );
 
-   void addMember( const Bestiole & b ) { listeBestioles.push_back(b); listeBestioles.back().initCoords(width, height); }
+   void addMember( const Bestiole & b ) { listeBestioles.push_back(b); listeBestioles.back().initCoords(width, height); } 
+   void removeMember ( ) { listeBestioles.erase(listeBestioles.begin()+1);};
    int nbVoisins( const Bestiole & b );
 
 };
